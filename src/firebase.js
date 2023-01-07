40ea0da4fba5943d87461dae67ed238227a895b8
Maverick Seneris <mavseneris@gmail.com>
2023-01-08 06:40:49 +0800

    intergrating file upload and storage handling

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
+import { getStorage } from "firebase/storage";
+
 // import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,4 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+
 // const analytics = getAnalytics(app);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import addAvatar from '../img/addAvatar.png'
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from '../firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 export default function Register() {
+    const [err, setErr] = useState(false)
 
-    function handleSumbit async (e) {
+    async function handleSumbit(e) {
         e.preventDefault();
         const displayName = e.target[0].value;
         const email = e.target[1].value;
         const password = e.target[2].value;
         const file = e.target[3].files[0];
 
-        try{
-              
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            const storageRef = ref(storage, displayName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                (error) => {
+                    setErr(true)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                        await updateProfile();
+                    });
+                }
+            );
+        } catch (err) {
+            setErr(true)
         }
-        const res = await createUserWithEmailAndPassword(auth, email, password)
-        
+
     }
 
-   
 
     return (
         <div className='formContainer'>
@@ -36,6 +51,7 @@ export default function Register() {
                     </label>
                     <input id="files" style={{ display: "none" }} type="file" multiple placeholder='add an avatar' />
                     <button>Sign up</button>
+                    {err && <span>Something went wrong.</span>}
                     <p>You do have an account? Login</p>
                 </form>
             </div>
